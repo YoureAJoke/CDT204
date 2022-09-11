6db3f4a28ad6272154711c1af7fd716cfe564642
YoureAJoke <98798563+YoureAJoke@users.noreply.github.com>
2022-09-11 16:50:57 +0200

    Update Program12.s

--- Program12.s
+++ Program12.s
@@ -1,60 +1,57 @@
 .globl main 
 .data
 array: .space 28 #array and its size
-Size: 24 #used to track index
-
-NL: .asciiz "\n" #new line string
+Size: 24 #maxsize
 Result: .asciiz "BIGGEST NUMBER IN ARRAY IS: " #string used for result
 .text
 
 main: 
-    lw $s0, Size #Max size
-    move $t0, $zero #Index counter
-    lw $t4, array($t0) #load first element into t4
-    addi $t0, $t0, 4 # increase index
+	#init registers for the first loop to get input from user then store into array
+    lw $s0, Size
+    move $t0, $zero 
+    lw $t4, array($t0) 
+    addi $t0, $t0, 4 
     
-Loop1:
-    # index counter < max size
-    slt $t9, $s0, $t0 #compare s0 to t0 then store true or false value into t9
-    bne $t9, $zero, Prepare #jump to prepare if t9 is true
-    li $v0, 5 # read integer 
+Loop_input: #gets user input which then stores it into array
+    # index counter < max size 
+    slt $t5, $s0, $t0 
+    bne $t5, $zero, Prepare #branch to label prepare if t5 not equal to zero (0) false
+    li $v0, 5  
     syscall
     sw $v0, array($t0) #store integer in array
     addi $t0, $t0, 4 #increase index
-    j Loop1
+    j Loop_input
     
-Prepare:
-	li $t0, 0 #init index to 0
-	lw $s0, Size #Max size
-    	move $t0, $zero #Index counter
-   	lw $t4, array($t0) #store first value of index into t4
-    	addi $t0, $t0, 4 #increase index size
-	j Loop
-Loop:
-			
-    slt $t1, $s0, $t0	#compare s0 to t0 store true or false value in t1
-    bne $t1, $zero, Exit_loop # if value is true junp to exit loop
-        
+Prepare: #re-init registers for second loop to find the largest value in the array
+    li $t0, 0
+    move $t0, $zero 
+    lw $t4, array($t0) #load first element into t4
+    addi $t0, $t0, 4 
+    j Loop_find_largest #unconditional branch to label loop_find_largest
     
+    		
+Loop_find_largest: #loops through array to find the largest integer value in the array
+    slt $t1, $s0, $t0	
+    bne $t1, $zero, Exit_loop # if value is true branches to exit loop
     lw $t2, array($t0) #load from array into t2
-    addi $t0, $t0, 4 #increase index 
+    addi $t0, $t0, 4 
     slt $t1, $t4, $t2 #compare t4 to t2 gets true or false value stored in t1
     bne $t1, $zero, Else #if true jumps to else
-    j Loop
+    j Loop_find_largest #unconditional branch to label loop_find_largest
     
 Else:
-    move $t4, $t2    #stores value of t2 into t4
+    move $t4, $t2
     j Loop    
     
    
          
-Exit_loop: 
+Exit_loop:  #label is used to print the largest value found and end the program
     li $v0, 4 #prints the result string
-    la $a0, Result #store string into argument
+    la $a0, Result 
     syscall 
     li $v0, 1 #prints integer value
-    move $a0, $t4 #store integer value into argument
+    move $a0, $t4 
     syscall
 
-    li $v0, 10 #exit
+    li $v0, 10 
     syscall
